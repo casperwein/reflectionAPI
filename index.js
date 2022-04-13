b981317533a109210bb2122140e3364b8a18bb42
casperWein <19201040.casperweinberger@gmail.com>
2022-04-13 21:07:38 +0700

    add user register, login, jwt

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+const userRouter = require("./router/user.js");
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
